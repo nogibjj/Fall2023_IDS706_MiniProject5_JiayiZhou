022e3aded5f180fac4e437afc82559cd1308b86a
JiayiZhou <jiayizhou@JiayiZhous-MacBook-Pro.local>
2023-09-30 16:51:46 -0400

    added

--- main.py
+++ main.py
@@ -1,19 +1,120 @@
-"""
-ETL-Query script
-"""
-
+"""handles cli commands"""
+import sys
+import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import query
+from mylib.query import (
+    update_record,
+    delete_record,
+    create_record,
+    general_query,
+    read_data,
+)
 
-# Extract
-print("Extracting data...")
-extract()
 
-# Transform and load
-print("Transforming data...")
-load()
+def handle_arguments(args):
+    """add action based on inital calls"""
+    parser = argparse.ArgumentParser(description="ETL-Query script")
+    parser.add_argument(
+        "action",
+        choices=[
+            "extract",
+            "transform_load",
+            "update_record",
+            "delete_record",
+            "create_record",
+            "general_query",
+            "read_data",
+        ],
+    )
+    args = parser.parse_args(args[:1])
+    print(args.action)
+    if args.action == "update_record":
+        parser.add_argument("record_id", type=int)
+        parser.add_argument("team")
+        parser.add_argument("year", type=int)
+        parser.add_argument("league")
+        parser.add_argument("goose_eggs", type=int)
+        parser.add_argument("broken_eggs", type=int)
+        parser.add_argument("mehs", type=int)
+        parser.add_argument("league_average_gpct", type=float)
+        parser.add_argument("ppf", type=float)
+        parser.add_argument("replacement_gpct", type=float)
+        parser.add_argument("gwar", type=float)
+        parser.add_argument("key_retro")
 
-# Query
-print("Querying data...")
-query()
+    if args.action == "create_record":
+        parser.add_argument("team")
+        parser.add_argument("year", type=int)
+        parser.add_argument("league")
+        parser.add_argument("goose_eggs", type=int)
+        parser.add_argument("broken_eggs", type=int)
+        parser.add_argument("mehs", type=int)
+        parser.add_argument("league_average_gpct", type=float)
+        parser.add_argument("ppf", type=float)
+        parser.add_argument("replacement_gpct", type=float)
+        parser.add_argument("gwar", type=float)
+        parser.add_argument("key_retro")
+
+    if args.action == "general_query":
+        parser.add_argument("query")
+
+    if args.action == "delete_record":
+        parser.add_argument("record_id", type=int)
+
+    # parse again with ever
+    return parser.parse_args(sys.argv[1:])
+
+
+def main():
+    """handles all the cli commands"""
+    args = handle_arguments(sys.argv[1:])
+
+    if args.action == "extract":
+        print("Extracting data...")
+        extract()
+    elif args.action == "transform_load":
+        print("Transforming data...")
+        load()
+    elif args.action == "update_record":
+        update_record(
+            args.record_id,
+            args.team,
+            args.year,
+            args.league,
+            args.goose_eggs,
+            args.broken_eggs,
+            args.mehs,
+            args.league_average_gpct,
+            args.ppf,
+            args.replacement_gpct,
+            args.gwar,
+            args.key_retro,
+        )
+    elif args.action == "delete_record":
+        delete_record(args.record_id)
+    elif args.action == "create_record":
+        create_record(
+            args.team,
+            args.year,
+            args.league,
+            args.goose_eggs,
+            args.broken_eggs,
+            args.mehs,
+            args.league_average_gpct,
+            args.ppf,
+            args.replacement_gpct,
+            args.gwar,
+            args.key_retro,
+        )
+    elif args.action == "general_query":
+        general_query(args.query)
+    elif args.action == "read_data":
+        data = read_data()
+        print(data)
+    else:
+        print(f"Unknown action: {args.action}")
+
+
+if __name__ == "__main__":
+    main()
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,26 +1,58 @@
 """
 Transforms and Loads data into the local SQLite3 database
-Example:
-,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
 """
 import sqlite3
 import csv
-import os
 
-#load the csv file and insert into a new sqlite3 database
-def load(dataset="/workspaces/sqlite-lab/data/GroceryDB_IgFPro.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
-    print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=''), delimiter=',')
-    conn = sqlite3.connect('GroceryDB.db')
+# load the csv file and insert into a new sqlite3 database
+def load(dataset="data/Goose.csv"):
+    """Transforms and Loads data into the local SQLite3 database"""
+    payload = csv.reader(open(dataset, newline=""), delimiter=",")
+    # skips the header of csv
+    next(payload)
+    conn = sqlite3.connect("Goose.db")
     c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
-    #insert
-    c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
+    c.execute("DROP TABLE IF EXISTS Goose")
+    c.execute(
+        """
+        CREATE TABLE Goose (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            year INTEGER,
+            team TEXT,
+            league TEXT,
+            goose_eggs INTEGER,
+            broken_eggs INTEGER,
+            mehs INTEGER,
+            league_average_gpct REAL,
+            ppf REAL,
+            replacement_gpct REAL,
+            gwar REAL,
+            key_retro TEXT
+        )
+    """
+    )
+    # insert
+    c.executemany(
+        """
+        INSERT INTO Goose (
+            name,
+            year,
+            team,
+            league,
+            goose_eggs,
+            broken_eggs,
+            mehs,
+            league_average_gpct,
+            ppf,
+            replacement_gpct,
+            gwar,
+            key_retro
+            ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+        payload,
+    )
     conn.commit()
     conn.close()
-    return "GroceryDB.db"
-
+    return "Goose.db"
